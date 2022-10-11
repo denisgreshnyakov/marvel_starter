d3a59ebbe3053b62a07342bbd984ad2c901f3b60
Denis <denisgreshnaykov@gmail.com>
2022-10-11 21:49:40 +0500

    add error boundary

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -1,17 +1,19 @@
 import img from "./error.gif";
 
 const ErrorMessage = () => {
-  <img
-    style={{
-      display: "block",
-      width: "250px",
-      height: "250px",
-      objectFit: "contain",
-      margin: "0 auto",
-    }}
-    src={img}
-    alt="Error"
-  />;
+  return (
+    <img
+      style={{
+        display: "block",
+        width: "250px",
+        height: "250px",
+        objectFit: "contain",
+        margin: "0 auto",
+      }}
+      src={img}
+      alt="Error"
+    />
+  );
 };
 
 export default ErrorMessage;
