abd5a8b4c581592bd22203a8d391caa70ca3eef3
Denis <denisgreshnaykov@gmail.com>
2022-11-05 22:09:47 +0500

    add CSSTransition

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 import PropTypes from "prop-types";
 
 import Spinner from "../spinner/Spinner";
@@ -14,6 +15,8 @@ const CharList = (props) => {
   const [charEnded, setCharEnded] = useState(false);
 
   const { loading, error, getAllCharacters } = useMarvelService();
+
+  const duration = 200;
 
   useEffect(() => {
     onRequest(offset, true);
@@ -61,25 +64,32 @@ const CharList = (props) => {
       }
 
       return (
-        <li
-          className="char__item"
+        <CSSTransition
           key={item.id}
-          tabIndex={0}
-          ref={(el) => (itemRefs.current[i] = el)}
-          onClick={() => {
-            props.onCharSelected(item.id);
-            focusOnItem(i);
-          }}
-          onKeyPress={(e) => {
-            if (e.key === " " || e.key === "Enter") {
+          in={newItemLoading}
+          timeout={duration}
+          classNames="char__item"
+        >
+          <li
+            className="char__item"
+            key={item.id}
+            tabIndex={0}
+            ref={(el) => (itemRefs.current[i] = el)}
+            onClick={() => {
               props.onCharSelected(item.id);
               focusOnItem(i);
-            }
-          }}
-        >
-          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
-          <div className="char__name">{item.name}</div>
-        </li>
+            }}
+            onKeyPress={(e) => {
+              if (e.key === " " || e.key === "Enter") {
+                props.onCharSelected(item.id);
+                focusOnItem(i);
+              }
+            }}
+          >
+            <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+            <div className="char__name">{item.name}</div>
+          </li>
+        </CSSTransition>
       );
     });
     // А эта конструкция вынесена для центровки спиннера/ошибки
@@ -90,7 +100,7 @@ const CharList = (props) => {
 
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading && !newItemLoading ? <Spinner /> : null;
-
+  console.log(newItemLoading);
   return (
     <div className="char__list">
       {errorMessage}
